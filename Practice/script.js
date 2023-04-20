80b900a23e3007be9d17da6215e81a0f3397b1aa
Adrian <adrianlica27@gmail.com>
2023-04-20 20:04:16 +0300

    practice

--- Practice/script.js
+++ Practice/script.js
@@ -1,3 +1,17 @@
-// fetch('https://api.github.com/users/adrianlica20')
-//   .then(response => response.json())
-//   .then(data => console.log(data.name, data.twitter_username, data.followers));
+fetch('http://httpstat.us/500')
+  .then(response => {
+    if (response.status === 404) {
+      throw new Error('Not Found');
+    } else if (response.status === 500) {
+      throw new Error('Server Error');
+    } else if (response.status !== 200) {
+      throw new Error('Request Failed');
+    }
+    return response;
+  })
+  .then(() => {
+    console.log('success');
+  })
+  .catch(error => {
+    console.log(error);
+  });
